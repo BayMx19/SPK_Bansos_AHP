20b2998a67a8f2364b3cef427fa8772c8c7c0bea
BayMx19 <isaimanmuhammad1@gmail.com>
2025-02-27 23:28:10 +0700

    Customized Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title mb-5">Home</h4>
+                <div class="card strpied-tabled-with-hover">
+                    <div class="card-header judul">
+                        <h4 class="card-title mb-2 text-bold">Home</h4>
                     </div>
                     <div class="card-body mt-5 mb-5">
                         <div class="welcome-text">
-                            <h1>Hallo, Selamat Datang {{ Auth::user()->name }}!</h1>
+                            <h1>Hallo, Selamat Datang <b>{{ Auth::user()->name }}!</b></h1>
                         </div>
                     </div>
                 </div>
